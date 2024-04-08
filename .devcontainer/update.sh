2820ae42e1ca5c2c534f49b66ff4229c697ca88d
Sidney Andrews <sidandrews@microsoft.com>
2024-04-08 23:37:31 +0000

    Install Azure Functions Core Tools

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -6,6 +6,9 @@ sudo dotnet workload update
 # Install SWA CLI
 npm install --global @azure/static-web-apps-cli
 
+# Install Azure Functions Core Tools
+npm install --global azure-functions-core-tools@4 --unsafe-perm true
+
 # Install emulator certificate
 curl -k https://cosmos-db:8081/_explorer/emulator.pem > ~/emulatorcert.crt
 sudo cp ~/emulatorcert.crt /usr/local/share/ca-certificates/
